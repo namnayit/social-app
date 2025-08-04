3cb9d2bf458b7692c6277f0c3ea1cf23fa8ae706
MH Shuvo <mhshuvoalways@gmail.com>
2025-08-04 12:59:01 +0600

    updated

--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -183,16 +183,6 @@ const ProfilePage = ({
             </button>
           </div>
 
-          <button className="new-event-btn">
-            <svg width="16" height="16" fill="currentColor" viewBox="0 0 20 20">
-              <path
-                fillRule="evenodd"
-                d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-                clipRule="evenodd"
-              />
-            </svg>
-            New Event
-          </button>
           <button
             className="new-event-btn"
             onClick={() => setShowEventModal(true)}
